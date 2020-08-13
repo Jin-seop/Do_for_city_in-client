f1df65d38cdcd0a052df50351c964cda9fe06f4f
JinSeop <turn3361@gmail.com>
2020-08-13 15:06:19 +0900

    :heavy_plus_sign: :imp: 게시판 페이지
     - 게시판 페이지 키보드 나올 때 페이지 밀림현상 미해결

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,30 +1,27 @@
 // import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function SignUp() {
   // const [userId, setUserId] = useState("");
   return (
     <View style={styles.container}>
-      <Text style={styles.logo}>DO.SI.IN{"\n"}회원가입</Text>
+      <Text style={styles.logo}>DO.SI.IN{'\n'}회원가입</Text>
       <View style={styles.idView}>
         <TextInput
           style={styles.idInputBox}
           placeholder="아이디"
           // onChangeText={(userId) => setUserId(text)}
           // defaultValue={userId}
-        ></TextInput>
+        />
         <TouchableOpacity style={styles.idCheckButton}>
           <Text style={styles.buttonText}>중복확인</Text>
         </TouchableOpacity>
       </View>
-      <TextInput style={styles.inputBox} placeholder="비밀번호"></TextInput>
-      <TextInput
-        style={styles.inputBox}
-        placeholder="비밀번호 재확인"
-      ></TextInput>
-      <TextInput style={styles.inputBox} placeholder="Email 입력"></TextInput>
+      <TextInput style={styles.inputBox} placeholder="비밀번호" />
+      <TextInput style={styles.inputBox} placeholder="비밀번호 재확인" />
+      <TextInput style={styles.inputBox} placeholder="Email 입력" />
       <TouchableOpacity style={styles.signUpButton}>
         <Text style={styles.buttonText}>가입하기</Text>
       </TouchableOpacity>
@@ -38,15 +35,15 @@ export default function SignUp() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   logo: {
     fontSize: 20,
-    textAlignVertical: "center",
-    textAlign: "center",
-    backgroundColor: "#F3ECA5",
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    backgroundColor: '#F3ECA5',
     width: 220,
     height: 144,
     borderRadius: 5,
@@ -55,46 +52,46 @@ const styles = StyleSheet.create({
   idView: {
     marginTop: 40,
     width: 220,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
   idInputBox: {
     width: 110,
     height: 30,
     borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "#888",
+    borderStyle: 'solid',
+    borderColor: '#888',
     borderRadius: 5,
   },
   idCheckButton: {
     width: 90,
-    backgroundColor: "#F3ECA5",
+    backgroundColor: '#F3ECA5',
     borderRadius: 5,
   },
   inputBox: {
     marginTop: 20,
     width: 220,
     borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: "#888",
+    borderStyle: 'solid',
+    borderColor: '#888',
     borderRadius: 5,
   },
   signUpButton: {
     marginTop: 60,
     width: 220,
-    backgroundColor: "#F3ECA5",
+    backgroundColor: '#F3ECA5',
     borderRadius: 5,
   },
   backButton: {
     marginTop: 25,
     width: 220,
-    backgroundColor: "#F3ECA5",
+    backgroundColor: '#F3ECA5',
     borderRadius: 5,
   },
   buttonText: {
-    textAlign: "center",
+    textAlign: 'center',
     height: 30,
-    textAlignVertical: "center",
+    textAlignVertical: 'center',
   },
 });
