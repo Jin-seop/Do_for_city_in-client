8626fcfc9a0a026d9a8d2da00317070b3103d006
JinSeop <turn3361@gmail.com>
2020-08-13 19:30:00 +0900

    :flags: 리액트 네이티브 네비게이션 패키지 설치
     -리액트 네이티브 화면 전환용 네비게이션 및 스텍 패키지 설치

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import LoginPage from './components/Login';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View>
+      <LoginPage />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
