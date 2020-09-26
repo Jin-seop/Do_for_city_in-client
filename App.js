ddc3562126aae85f31759c184677d13adfab24bd
JinSeop <turn3361@gmail.com>
2020-09-26 16:28:07 +0900

    삭제 : 프로젝트 초기화
     - 프로젝트 css변경을 위한 프로젝트 초기화

--- App.js
+++ App.js
@@ -1,48 +1,9 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
-import Login from './components/Login';
-import SignUp from './components/SignUp';
-import MainPage from './components/MainPage';
-import Mypage from './components/Mypage';
-import PostPage from './components/Postpage';
-import WritePage from './components/Write';
-import ErrorPage from './components/ErrorPage';
-import EditUserInfo from './components/EditUserInfo';
 
-const Route = createStackNavigator(
-  {
-    Login: {
-      screen: Login,
-    },
-    SignUp: {
-      screen: SignUp,
-    },
-    MainPage: {
-      screen: MainPage,
-    },
-    Mypage: {
-      screen: Mypage,
-    },
-    PostPage: {
-      screen: PostPage,
-    },
-    WritePage: {
-      screen: WritePage,
-    },
-    EditUserInfo: {
-      screen: EditUserInfo,
-    },
-    ErrorPage: {
-      screen: ErrorPage,
-    },
-  },
-  {
-    defaultNavigationOptions: {
-      headerShown: false,
-      title: null,
-    },
-  }
-);
+// const Route = createStackNavigator(
+  
+// );
 
 const App = createAppContainer(Route);
 
