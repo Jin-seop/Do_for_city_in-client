7f578548ef4c7626efbd08add5a76dfa624eac24
JinSeop <turn3361@gmail.com>
2020-09-27 00:37:02 +0900

    추가 : App.js  네비게이션 적용
      - 스텍 네비게이션 적용완료
      - 탭 네비게이션 적용완료

--- App.js
+++ App.js
@@ -1,9 +1,24 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function Navigation() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="홈" component={Home} />
+      <Tab.Screen name="카테고리" component={Kategorie} />
+      <Tab.Screen name="마이페이지" component={Mypage} />
+      <Tab.Screen name="Cook.Si.In" component={CookSiIn} />
+    </Tab.Navigator>
+  );
+}
+
+function Home() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       <Text>Home!</Text>
@@ -35,17 +50,15 @@ function CookSiIn() {
   );
 }
 
-const Tab = createBottomTabNavigator();
-
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="홈" component={HomeScreen} />
-        <Tab.Screen name="카테고리" component={Kategorie} />
-        <Tab.Screen name="마이페이지" component={Mypage} />
-        <Tab.Screen name="Cook.Si.In" component={CookSiIn} />
-      </Tab.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Navigation" component={Navigation} />
+        <Stack.Screen name="Kategorie" component={Kategorie} />
+        <Stack.Screen name="Mypage" component={Mypage} />
+        <Stack.Screen name="CookSiIn" component={CookSiIn} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
