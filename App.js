a1380aa0147823e2e7c41f050c4824732fedbb33
JinSeop <turn3361@gmail.com>
2020-09-26 22:25:05 +0900

    추가 : 탭 네비게이션 설치

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator()
 
 // const Route = createStackNavigator(
   
